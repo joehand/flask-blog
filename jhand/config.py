811f8c7da336acec608d75b79e840658faf93ee6
Joe Hand <joe.a.hand@gmail.com>
2014-06-28 23:39:09 -0600

    update cache

--- jhand/config.py
+++ jhand/config.py
@@ -21,8 +21,8 @@ class Config(object):
     S3_BUCKET_NAME = 'joehand_blog'
 
     S3_HEADERS = {
-        'Expires': 'Thu, 15 Feb 2015',
-        'Cache-Control': 'max-age=86400',
+        'Expires': '21 Jun 2015',
+        'Cache-Control': 'max-age=31536000',
     }
 
     SECURITY_PASSWORD_HASH = 'bcrypt'
